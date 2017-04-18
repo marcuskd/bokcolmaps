8468b69ec101a9010c28ad2f0a712054fb839f42
Marcus Donnelly <marcus.k.donnelly@gmail.com>
2017-04-18 10:28:31 +0100

    Need Tornado 4.4.2, problem with 4.5

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(name = 'bokcolmaps',
                   ],
       packages = find_packages(),
       install_requires = ['numpy >= 1.12',
+                          'tornado == 4.4.2',
                           'bokeh >= 0.12.5'
                           ],
       package_data = {'bokcolmaps':['jet.txt'],
